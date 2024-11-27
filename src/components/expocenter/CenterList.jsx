160cb63cfdec1ef0bc68ca524d17cee0667b0309
mustufa-kashif370 <117252681+mustufa-kashif370@users.noreply.github.com>
2024-11-28 00:09:33 +0500

    Updated CityList.jsx

--- src/components/expocenter/CenterList.jsx
+++ src/components/expocenter/CenterList.jsx
@@ -1,72 +1,224 @@
 import React from "react";
-
+import EventBG from "../../assets/img/serviceBG.png";
 function CenterList() {
   return (
     <>
-      <div className="row gy-4 justify-content-center p-60">
-
-
-
-        <div className="col-lg-3 col-md-6 pt-lg-3 pt-md-2 pt-sm-1" >
-          <div className="" >
-            <p style={{ color: "darkmagenta", fontSize: '20px', fontWeight: 'bolder' }}>Karachi</p>
-            <ul class="p-2 text-sm max-w-xs ">
-              <li class="py-2">
-                The Karachi Expo Centre
-                <ul class="list-disc list-outside ps-5 mt-2 space-y-1 ">
-                  <li>Exhibition Hall No. 1</li>
-                  <li>Exhibition Hall No. 2</li>
-                  <li>Exhibition Hall No. 3</li>
-                  <li>Foyer Area</li>
-                  <li>Parkings</li>
-                  <li>Ground Floor Hall, offices, ticket booths, Information center,Waiting Area</li>
-                  <li>Restaurant, Coffee shop, Office and Toilets</li>
-                 
-
-                </ul>
-              </li>
-
-            </ul>
+      <section id="service">
+        <div
+          className="bh_service_wrapper"
+          style={{ backgroundImage: `url(${EventBG})` }}
+        >
+          <div className="bh_service_head">
+            {/* <div className="bh_ser_top">Events</div> */}
+            <h3>Center List</h3>
+          </div>
+          <div className="container">
+            <div className="row gy-4 justify-content-center p-60">
+              <div className="col-lg-4 col-md-6 pt-lg-3 pt-md-2 pt-sm-1">
+                <div className="">
+                  <p
+                    style={{
+                      color: "darkmagenta",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Karachi
+                  </p>
+                  <ul className="p-2 text-sm max-w-xs list list-disc">
+                    <li className="py-2">
+                      <a
+                        href="https://tdap.gov.pk/expo-center-karachi/"
+                        target="_blank"
+                        style={{backgroundColor:"rgba(148, 49, 145, 0.5)"}}> &nbsp; &nbsp; &nbsp; 
+                     
+                        The Karachi Expo Centre &nbsp; &nbsp; &nbsp; 
+                      </a>
+                      <ul className="list-disc list-outside ps-5 mt-2 space-y-1 ">
+                        <li>Exhibition Hall No. 1</li>
+                        <li>Exhibition Hall No. 2</li>
+                        <li>Exhibition Hall No. 3</li>
+                        <li>Foyer Area</li>
+                        <li>Parkings</li>
+                        <li>
+                          Ground Floor Hall, offices, ticket booths, Information
+                          center,Waiting Area
+                        </li>
+                        <li>Restaurant, Coffee shop, Office and Toilets</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 pt-lg-3 pt-md-2 pt-sm-1">
+                <div className="">
+                  <p
+                    style={{
+                      color: "darkmagenta",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Islamabad
+                  </p>
+                  <ul className="p-2 text-sm max-w-xs list list-disc">
+                    <li className="py-2">
+                      <a href="https://conventioncenter.pk/" target="_blank"
+                     
+                        style={{backgroundColor:"rgba(148, 49, 145, 0.5)"}}> &nbsp; &nbsp; &nbsp; 
+                        Jinnah Convention Center &nbsp; &nbsp; &nbsp; 
+                      </a>
+                      <ul className="list-disc list-outside ps-5 mt-2 space-y-1 ">
+                        <li>Exhibition Halls</li>
+                        <li>Convention Halls</li>
+                        <li>Three Parking lots</li>
+                        <li>
+                          Ground Floor Hall, offices, ticket booths, Information
+                          center,Waiting Area
+                        </li>
+                        <li>Restaurant, Coffee shop, Office and Toilets</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 pt-lg-3 pt-md-2 pt-sm-1">
+                <div className="">
+                  <p
+                    style={{
+                      color: "darkmagenta",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Lahore
+                  </p>
+                  <ul className="p-2 text-sm max-w-xs list list-disc">
+                    <li className="py-2 ">
+                      <a
+                        href="https://www.pakexcel.com/expo-centre-lahore"
+                        target="_blank"
+                     
+                        style={{backgroundColor:"rgba(148, 49, 145, 0.5)"}}> &nbsp; &nbsp; &nbsp; 
+                        Lahore International Expo Centre &nbsp; &nbsp; &nbsp; 
+                      </a>
+                      <ul className="list-disc list-outside ps-5 mt-2 space-y-1 ">
+                        <li>Exhibition Hall 01, Hall 02, Hall 03</li>
+                        <li>Convention Hall, Auditorium 01, 02</li>
+                        <li>Foyer, Summit Room, Meeting Rooms</li>
+                        <li>General Hall A, Hall B</li>
+                        <li>Open Event Area</li>
+                        <li>
+                          Ground Floor Hall, offices, ticket booths, Information
+                          center,Waiting Area
+                        </li>
+                        <li>Restaurant, Coffee shop, Office and Toilets</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 pt-lg-3 pt-md-2 pt-sm-1">
+                <div className="">
+                  <p
+                    style={{
+                      color: "darkmagenta",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Peshawar
+                  </p>
+                  <ul className="p-2 text-sm max-w-xs list list-disc">
+                    <li className="py-2 ">
+                      <a
+                        href="https://pakexcel.com/expo-centre-peshawar"
+                        target="_blank"
+                        style={{backgroundColor:"rgba(148, 49, 145, 0.5)"}}>  &nbsp; &nbsp; &nbsp;
+                        Expo Centre Peshawar &nbsp; &nbsp; &nbsp;
+                      </a>
+                      
+                      
+                      <ul className="list-disc list-outside ps-5 mt-2 space-y-1 ">
+                        <li>Exhibition Hall 01, Hall 02</li>
+                        <li>Convention Hall 01</li>
+                        <li>Open Exhibition Areas</li>
+                        <li>Utility Building cum Admin Block 01 </li>
+                        <li> Power House 01</li>
+                        <li>Ware House 01, House 02</li>
+                        <li>Gate Houses 06</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 pt-lg-3 pt-md-2 pt-sm-1">
+                <div className="">
+                  <p
+                    style={{
+                      color: "darkmagenta",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Multan
+                  </p>
+                  <ul className="p-2 text-sm max-w-xs list list-disc">
+                    <li className="py-2 ">
+                      <a
+                        href="https://expostore.pk/?srsltid=AfmBOopeOm7LhcjR1DfWjEiZ2jaDhti_OMDkMAVavYElxz5b2s8mG2R7"
+                        target="_blank"
+                        style={{backgroundColor:"rgba(148, 49, 145, 0.5)"}}> &nbsp; &nbsp; &nbsp; 
+                        Multan Expo Store  &nbsp; &nbsp; &nbsp; 
+                      </a>
+                      <ul className="list-disc list-outside ps-5 mt-2 space-y-1 ">
+                        <li>Exhibition Hall 01, Hall 02, Hall 03</li>
+                        <li>Convention Hall, Auditorium 01, 02</li>
+                        <li>Foyer, Summit Room, Meeting Rooms</li>
+                        <li>General Hall A, Hall B</li>
+                        <li>Open Event Area</li>
+                        <li>
+                          Ground Floor Hall, offices, ticket booths, Information
+                          center,Waiting Area
+                        </li>
+                        <li>Restaurant, Coffee shop, Office and Toilets</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 pt-lg-3 pt-md-2 pt-sm-1">
+                <div className="">
+                  <p
+                    style={{
+                      color: "darkmagenta",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Quetta
+                  </p>
+                  <ul className="p-2 text-sm max-w-xs list list-disc">
+                    <li className="py-2 " >
+                      <a href="https://www.pakexcel.com/expo-centre-quetta" target="_blank" style={{backgroundColor:"rgba(148, 49, 145, 0.5)"}}> &nbsp; &nbsp; &nbsp; Expo Centre Quetta&nbsp; &nbsp; &nbsp; </a>
+                      <ul className="list-disc list-outside ps-5 mt-2 space-y-1 ">
+                        <li>Exhibition Hall 01, Hall 02</li>
+                        <li>Convention Hall 01</li>
+                        <li>Open Exhibition Areas</li>
+                        <li>Utility Building cum Admin Block 01 </li>
+                        <li> Power House 01</li>
+                        <li>Ware House 01, House 02</li>
+                        <li>Gate Houses 06</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6 pt-lg-3 pt-md-2 pt-sm-1" >
-          <div className="" >
-            <p style={{ color: "darkmagenta", fontSize: '20px', fontWeight: 'bolder' }}>Karachi</p>
-            <ul class="p-2 text-sm max-w-xs ">
-              <li class="py-2">
-                The Karachi Expo Centre
-                <ul class="list-disc list-outside ps-5 mt-2 space-y-1 ">
-                  <li>Interactive, Analytics & Insights</li>
-                  <li> Engineering, Industrial Services</li>
-                  <li>Internet Of Things</li>
-
-                </ul>
-              </li>
-
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 pt-lg-3 pt-md-2 pt-sm-1" >
-          <div className="" >
-            <p style={{ color: "darkmagenta", fontSize: '20px', fontWeight: 'bolder' }}>Karachi</p>
-            <ul class="p-2 text-sm max-w-xs ">
-              <li class="py-2">
-                The Karachi Expo Centre
-                <ul class="list-disc list-outside ps-5 mt-2 space-y-1 ">
-                  <li>Interactive, Analytics & Insights</li>
-                  <li> Engineering, Industrial Services</li>
-                  <li>Internet Of Things</li>
-
-                </ul>
-              </li>
-
-            </ul>
-          </div>
-        </div>
-
-      </div>
+      </section>
     </>
-
   );
 }
 
