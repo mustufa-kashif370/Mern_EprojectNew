e4df533dea62fcef935d1fbe13f2ed8e547ed375
mustufa-kashif370 <117252681+mustufa-kashif370@users.noreply.github.com>
2024-11-30 17:15:45 +0500

    Update About_Banner.jsx

--- src/components/about/About_Banner.jsx
+++ src/components/about/About_Banner.jsx
@@ -1,90 +1,84 @@
-import React from 'react'
+import React from "react";
 import "../../assets/css/home.css";
 import "../../assets/css/main.css";
 import "../../assets/css/style.css";
-import bannervector2 from '../../assets/img/bannervector2.png'
-import bannervector1 from '../../assets/img/bannervector1.png'
-import Events2 from '../../assets/img/Events2.png'
-import bannerbackground from '../../assets/img/bannerBG.png'
+import bannervector2 from "../../assets/img/bannervector2.png";
+import bannervector1 from "../../assets/img/bannervector1.png";
+import Events2 from "../../assets/img/Events2.png";
+import bannerbackground from "../../assets/img/bannerBG.png";
 function Event_Banner() {
   return (
     <>
-    <div className="first_wrap">
-       
+      <div className="first_wrap">
+        {/* Banner Section */}
+        <section
+          id="home"
+          className="bh_banner_wrapper"
+          style={{
+            backgroundImage: `url(${bannerbackground})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="container">
+            <div className="row gy-4 justify-content-center p-60">
+              {/* Left Content */}
+              <div className="col-lg-6 col-md-12 flex_align_center">
+                <div className="banner_content_left">
+                  {/* <h5>Hello Welcome!!</h5> */}
+                  <h3>
+                    With <span> Event Sphere</span>
+                  </h3>
+                  <h3>
+                    Organize, Attend and Explore{" "}
+                    <span>Events In Different Cities</span>
+                  </h3>
+                  <p>
+                    From local expos to global trade shows, discover what
+                    excites you anytime, anywhere.
+                  </p>
+                  <div className="bh_btn">
+                    <button
+                      className="btn btn btn-large buttonattend"
+                      href="#plan"
+                      data-section="plan"
+                    >
+                      Attend
+                    </button>
+                  </div>
+                  &nbsp;
+                  <div className="bh_btn">
+                    <button
+                      className="btn btn btn-large buttonhost"
+                      href="#plan"
+                      data-section="plan"
+                    >
+                      Host
+                    </button>
+                  </div>
+                </div>
+              </div>
 
-       {/* Banner Section */}
-       <section
- id="home"
- className="bh_banner_wrapper"
- style={{
-   backgroundImage: `url(${bannerbackground})`,
-   backgroundSize: "cover",
-   backgroundPosition: "center",
-   
-   
- }}
->
-
-         <div className="container">
-           <div className="row gy-4 justify-content-center p-60">
-             {/* Left Content */}
-             <div className="col-lg-6 col-md-12 flex_align_center">
-               <div className="banner_content_left">
-                 {/* <h5>Hello Welcome!!</h5> */}
-                 <h3>
-                   With  <span> Event Sphere</span>
-                 </h3>
-                 <h3>
-                 Organize, Attend and Explore <span>Events In Different Cities</span>
-                 </h3>
-                 <p>
-                   From local expos to global trade shows, discover what
-                   excites you anytime, anywhere.
-                 </p>
-
-                 <div className="bh_btn">
-                   <button className='btn btn btn-large buttonattend' href="#plan" data-section="plan">
-                     Attend
-                   </button>
-                 </div>
-                 &nbsp;
-                 <div className="bh_btn">
-                   <button className='btn btn btn-large buttonhost'  href="#plan" data-section="plan">
-                     Host
-                   </button>
-                 </div>
-               </div>
-             </div>
-
-             {/* Right Image Section */}
-             <div className="col-lg-6 col-md-12 text-end relative">
-               <div>
-                 <img
-                   src={Events2}
-                   alt="banner side img"
-                   style={{
-                    borderRadius:'30px'
-                   }}
-                 />
-                 <img
-                   src={bannervector1}
-                   alt="vector"
-                   className="vector1"
-                   
-                 />
-                 <img
-                   src={bannervector2}
-                   alt="vector"
-                   className="vector2"
-                 />
-               </div>
-             </div>
-           </div>
-         </div>
-       </section>
-     </div>
+              {/* Right Image Section */}
+              <div className="col-lg-6 col-md-12 text-end relative">
+                <div>
+                  <img
+                    src={Events2}
+                    alt="banner side img"
+                    style={{
+                      borderRadius: "30px",
+                    }}
+                  />
+                  <img src={bannervector1} alt="vector" className="vector1" />
+                  <img src={bannervector2} alt="vector" className="vector2" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default Event_Banner
+export default Event_Banner;
