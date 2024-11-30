034db7ba2a423d7934e1ef33ea62c35375a6fe7f
mustufa-kashif370 <117252681+mustufa-kashif370@users.noreply.github.com>
2024-11-30 22:31:38 +0500

    updated all

--- src/components/BannerComponents.jsx
+++ src/components/BannerComponents.jsx
@@ -1,80 +1,64 @@
-import React from 'react'
+import React from "react";
 import "../assets/css/home.css";
 import "../assets/css/main.css";
 import "../assets/css/style.css";
-import bannervector2 from '../assets/img/bannervector2.png'
-import bannervector1 from '../assets/img/bannervector1.png'
-import bannergirls from '../assets/img/bannergirls.png'
-import bannerbackground from '../assets/img/bannerBG.png'
+import bannervector2 from "../assets/img/bannervector2.png";
+import bannervector1 from "../assets/img/bannervector1.png";
+import bannergirls from "../assets/img/bannergirls.png";
+import bannerbackground from "../assets/img/bannerBG.png";
 function BannerComponents() {
   return (
     <>
-    <div className="first_wrap">
-       
+      <div className="first_wrap">
+        {/* Banner Section */}
+        <section
+          id="home"
+          className="bh_banner_wrapper"
+          style={{
+            backgroundImage: `url(${bannerbackground})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="container">
+            <div className="row gy-4 justify-content-center p-60">
+              {/* Left Content */}
+              <div className="col-lg-6 col-md-12 flex_align_center">
+                <div className="banner_content_left">
+                  {/* <h5>Hello Welcome!!</h5> */}
+                  <h3>
+                    Event Sphere <span>Organize, Attend and Explore</span>
+                  </h3>
+                  <h3>
+                    In <span>Multiple Cities</span>
+                  </h3>
+                  <p>
+                    From local expos to global trade shows, discover what
+                    excites you anytime, anywhere.
+                  </p>
 
-       {/* Banner Section */}
-       <section
- id="home"
- className="bh_banner_wrapper"
- style={{
-   backgroundImage: `url(${bannerbackground})`,
-   backgroundSize: "cover",
-   backgroundPosition: "center",
-   
-   
- }}
->
-
-         <div className="container">
-           <div className="row gy-4 justify-content-center p-60">
-             {/* Left Content */}
-             <div className="col-lg-6 col-md-12 flex_align_center">
-               <div className="banner_content_left">
-                 {/* <h5>Hello Welcome!!</h5> */}
-                 <h3>
-                   Event Sphere <span>Organize, Attend and Explore</span>
-                 </h3>
-                 <h3>
-                   In <span>Multiple Cities</span>
-                 </h3>
-                 <p>
-                   From local expos to global trade shows, discover what
-                   excites you anytime, anywhere.
-                 </p>
-
-                 <div className="bh_btn">
-                   <a href="#plan" data-section="plan">
+                  <div className="bh_btn">
+                   <button className='btn btn btn-large buttongetstarted' href="#plan" data-section="plan">
                      Get Started
-                   </a>
+                   </button>
                  </div>
-               </div>
-             </div>
+                </div>
+              </div>
 
-             {/* Right Image Section */}
-             <div className="col-lg-6 col-md-12 text-end relative">
-               <div>
-                 <img
-                   src={bannergirls}
-                   alt="banner side img"
-                 />
-                 <img
-                   src={bannervector1}
-                   alt="vector"
-                   className="vector1"
-                 />
-                 <img
-                   src={bannervector2}
-                   alt="vector"
-                   className="vector2"
-                 />
-               </div>
-             </div>
-           </div>
-         </div>
-       </section>
-     </div>
+              {/* Right Image Section */}
+              <div className="col-lg-6 col-md-12 text-end relative">
+                <div>
+                  <img src={bannergirls} alt="banner side img" />
+                  <img src={bannervector1} alt="vector" className="vector1" />
+                  <img src={bannervector2} alt="vector" className="vector2" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default BannerComponents
+export default BannerComponents;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,88 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/home.css";
 import "../assets/css/main.css";
 import "../assets/css/style.css";
-import TopLogo from '../assets/icons/1/ticket.svg'
+import TopLogo from "../assets/icons/1/ticket.svg";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-     <header className="bh_header_wrapper" >
-          {/* <div className="bh_head_overlay"></div> */}
-          <div className="container">
-            <div className="bh_flex_menu" id="header">
-              <div
-                className="bh_logo"
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src={TopLogo}
-                  alt="Logo"
-                  style={{ height: "40px" }} // Optional
-                />&nbsp;
-                <h2
-                  className="logo_heading"
-                  style={{ marginTop: "0px", fontWeight: "500" }}
-                >
-                  Event Sphere
-                </h2>
-              </div>
+      <header className="bh_header_wrapper">
+        <div className="container">
+          <div className="bh_flex_menu" id="header">
+            {/* Logo Section */}
+            <div
+              className="bh_logo"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <img src={TopLogo} alt="Logo" style={{ height: "40px" }} />
+              &nbsp;
+              <p className="logo_heading">Event Sphere</p>
+            </div>
 
-              {/* Navigation Menu */}
-              <nav className="bh_menu">
-                <ul>
-                  <li>
-                    <Link  to={"/"} >Home</Link >
-                  </li>
-                  <li>
-                  <Link  to={"/events"} >Events</Link>
-                  </li>
-                  <li>
-                  <Link  to={"/expo-center"} >Expo Centers</Link>
-                  </li>
-                  <li className="dropdown">
-                    <a
-                      href="#"
-                      className="dropdown-toggle"
-                      id="dropdownMenuButton"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      More
-                    </a>
-                    <ul
-                      className="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton"
-                    >
-                      <li><Link className="dropdown-item" to={'/about'}>About</Link></li>
-                      <li><Link className="dropdown-item" to={'/attend'}>Attend</Link></li>
-                      <li><Link className="dropdown-item" to={'/host'}>Host</Link></li>
-                      <li><Link className="dropdown-item" to={'/activities'}>Activities</Link></li>
-                      <li><Link className="dropdown-item" to={'/contact'}>Contact Us</Link></li>
-                    </ul>
-                  </li>
-                </ul>
-
-                <div className="btn_login  ">
-                  <Link href="/login">Login</Link>
+            {/* Navigation Menu */}
+            <nav className={`bh_menu ${isMenuOpen ? "menu_open" : ""}`}>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/events">Events</Link>
+                </li>
+                <li>
+                  <Link to="/expo-center">Expo Centers</Link>
+                </li>
+                <li className="dropdown">
+                  <a
+                    href="#"
+                    className="dropdown-toggle"
+                    id="dropdownMenuButton"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    More
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <li>
+                      <Link className="dropdown-item" to="/about">
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/attend">
+                        Attend
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/host">
+                        Host
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/activities">
+                        Activities
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/contact">
+                        Contact Us
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <div className="btn_login">
+                  <Link to="/login">Login</Link>
                 </div>
+              </ul>
+            </nav>
 
-                <div className="bh_toggle" id="tog_btn">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </nav>
+            {/* Hamburger Toggle Button */}
+            <div className="bh_toggle" id="tog_btn" onClick={toggleMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
           </div>
-        </header>
-      
+        </div>
+      </header>
     </>
   );
 }
